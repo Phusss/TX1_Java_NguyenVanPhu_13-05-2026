d0661817c173e92f39864743adc6579fd647f9ec
Nguyen Van Phu <phu15012005@gmail.com>
2026-05-22 23:50:32 +0700

    Nộp bài kiểm tra thường xuyên 2

--- src/main/java/Housing.java
+++ src/main/java/Housing.java
@@ -1,11 +1,12 @@
-public class Housing extends Product {
+import java.io.Serializable;
+
+public class Housing extends Product implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private double area;
     private String address;
 
-    public Housing() {
-        super();
-    }
+    public Housing() { super(); }
 
     public Housing(String product_id, String product_name, double product_price, int product_total, double area, String address) {
         super(product_id, product_name, product_price, product_total);
@@ -15,14 +16,11 @@ public class Housing extends Product {
 
     public double getArea() { return area; }
     public void setArea(double area) { this.area = area; }
-
     public String getAddress() { return address; }
     public void setAddress(String address) { this.address = address; }
 
     @Override
     public String toString() {
-        return "Housing [" + super.toString() +
-                ", Area: " + area +
-                ", Address: " + address + "]";
+        return super.toString() + ", Area: " + area + ", Address: " + address;
     }
 }
